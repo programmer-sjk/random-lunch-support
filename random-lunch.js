589b4d73a77913ba9710e80e40372c80b49c3da4
sjk5766 <wjdrnr6662@gmail.com>
2024-02-05 08:15:42 +0900

    feat: 엑셀 읽은 후 멤버를 분리하는 함수들 추가

--- random-lunch.js
+++ random-lunch.js
@@ -22,4 +22,88 @@ process.stdin.on('keypress', async (_, data) => {
   }
 });
 
-console.log('Press a key');
+function shuffleMembers(members) {
+  return members.sort(() => Math.random() - 0.5);
+}
+
+const members = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+];
+
+function splitMembers(members) {
+  const results = [];
+  const limit = 4;
+  const memberCount = members.length;
+  let fourMemberTeamCount, threeMemberTeamCount, fourTeamCount;
+
+  const remainder = memberCount % limit;
+  switch (remainder) {
+    case 0:
+    case 3:
+      for (let i = 0; i < memberCount; i += limit) {
+        results.push(members.slice(i, i + limit));
+      }
+      break;
+    case 1:
+      fourMemberTeamCount = Math.floor(memberCount / limit) - 2;
+      threeMemberTeamCount = 3;
+      for (let i = 0; i < fourMemberTeamCount; i++) {
+        const start = i * limit;
+        results.push(members.slice(start, start + limit));
+      }
+
+      fourTeamCount = results.length;
+      for (let i = 0; i < threeMemberTeamCount; i++) {
+        const start = fourTeamCount * 4 + i * 3;
+        const eachTeam = members.slice(start, start + 3);
+        if (eachTeam.length) {
+          results.push(eachTeam);
+        }
+      }
+      break;
+    case 2:
+      fourMemberTeamCount = Math.floor(memberCount / limit) - 1;
+      threeMemberTeamCount = 2;
+      for (let i = 0; i < fourMemberTeamCount; i++) {
+        const start = i * limit;
+        results.push(members.slice(start, start + limit));
+      }
+
+      fourTeamCount = results.length;
+      for (let i = 0; i < threeMemberTeamCount; i++) {
+        const start = fourTeamCount * 4 + i * 3;
+        const eachTeam = members.slice(start, start + 3);
+        if (eachTeam.length) {
+          results.push(eachTeam);
+        }
+      }
+      break;
+  }
+
+  return results;
+}
+
+function createMessage(members) {
+  let message = '오늘의 랜덤런치!';
+  for (const member of members) {
+    message += `\n * ${member.join(', ')}`;
+  }
+  return message;
+}
+
+function showMessage(message) {
+  console.log('안녕하세요. HR 담당자님! 오늘의 랜덤 런치 결과입니다!');
+  console.log(message);
+}
